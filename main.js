fc474029a42c8fa83316ff3925a2164994eaa392
EdgarXK <edgar.sotelo.arceo@gmail.com>
2020-10-26 15:16:25 -0600

    Backend funcionando

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express= require('express');
 const bodyParser= require('body-parser');
 var mongoose = require('mongoose');
 const router=require('./src/routes/routes');
-
+const cors=require('cors');
 
 
 
@@ -14,17 +14,12 @@ var app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+
+app.use(cors());
 app.use(router);
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-
-
- mongoose.connect("mongodb://localhost/Asana");
+ 
+mongoose.connect("mongodb://localhost/Asana");
 
 
 
