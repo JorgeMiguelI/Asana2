fb488efdf64b1c10fbbd42fa347a3a4c9deaf32a
Jorge Miguel Ibarra Ortiz <jorgemgibarraortiz@gmail.com>
2020-12-02 14:17:17 -0600

    Se agrega api para mandar correo con codigo para unirse a empresa

--- src/routes/Colaborador.js
+++ src/routes/Colaborador.js
@@ -1,6 +1,8 @@
 //Arhcio donde estaran todas las apis para trabajar con los Colaboradores
 const express= require('express');
 const Router= express.Router();
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport')
 
 var Empresa = require('../Schema/Empresa');
 var User = require('../Schema/Usuario');
@@ -30,6 +32,62 @@ Router.get('/UpdateRol/:idColaborador', async (req, res)=>{
    
 
 })
+
+//Api para mandar correos para que se unan a una empresa
+Router.post('/SendMail', async(req, res)=>{
+    const datos= req.body.data;
+    const idEmpresa= datos.empresa;
+    const ListaCorreos= datos.Correos;
+
+    try{
+        let empresa= await Empresa.findById(idEmpresa);
+        //console.log(empresa);
+        for(let correo of ListaCorreos){
+            await SendCorreo(correo, empresa);
+        }
+        res.json({msg: "Ok"});
+
+    }catch(e){
+        console.log(e)
+        res.json({msg: "error"})
+    }
+    
+});
+
+async function SendCorreo(correo, empresa){
+    
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        auth: {
+            user: 'teamazana2@gmail.com',
+            pass: 'TeamAzana12345'
+        }
+    }));
+    var mailOptions = {
+        from: 'teamazana2@gmail.com',
+        to: correo, //debe de ir el correo a quien se le enciara
+        subject: "Se te ha pedido unirte a una Empresa",
+        html: "<h1 style='color: red;'>Asana2</h1>" +
+         "<p>" + "Saludos del equipo de Azana2, has sido invitado a formar parte de la empresa "+ "<b>"+ empresa.nombre +"</b> ."+
+         "Registrate en nuestra plataforma e ingresa el siguiente codigo para que te unas a esta empresa." + "</p><br>"+
+         "<h4><b>Codigo: "+ empresa._id +"</b> </h4>"
+    };
+    await transporter.sendMail(mailOptions)
+    .then(data=>{
+        console.log("Mensaje mandado a "+ correo);
+    })
+    .catch(e=>{
+        console.log(e);
+    })
+    /*transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Mensaje mandado a "+ correo);
+        }
+    });*/
+
+}
 
 
 module.exports=Router;
